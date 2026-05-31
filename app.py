dbc7c99f6b6911b4b3358dfd30aebf51d1f49c04
IAMLATI <117564925+IAMLATI@users.noreply.github.com>
2026-05-31 12:34:36 +0000

    fixed connection to render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -11,5 +12,8 @@ def predict():
     prediction = model.predict([data])
     return jsonify({"prediction": float(prediction[0])})
 
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
