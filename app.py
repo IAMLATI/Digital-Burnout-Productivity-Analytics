76a6f0a581637cedbd2c29b68e27c56d14cba1f8
IAMLATI <117564925+IAMLATI@users.noreply.github.com>
2026-05-31 13:24:47 +0000

    testing the model

--- app.py
+++ app.py
@@ -10,9 +10,12 @@ model = joblib.load("XGBoost_model.pkl")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.json["features"]
-    prediction = model.predict([data])
-    return jsonify({"prediction": float(prediction[0])})
+    data = request.json
+    return jsonify(data)
+    # data = request.json["features"]
+    # prediction = model.predict([data])
+    # return jsonify({"prediction": float(prediction[0])})
+
 
 
 if __name__ == "__main__":
